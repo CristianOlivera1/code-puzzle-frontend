4e8d532953c9c2d6855d1979c01d60469ca2de5f
cristianolivera1 <221181@unamba.edu.pe>
2025-07-17 17:48:59 -0500

    Refactor code structure for improved readability and maintainability

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Header } from '../../shared/header/header';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [Header],
+  imports: [RouterLink],
   templateUrl: './login.html',
   styleUrls: ['./login.css']
 })
--- src/app/pages/register/register.ts
+++ src/app/pages/register/register.ts
@@ -2,27 +2,14 @@ import { Component } from '@angular/core';
 import { Header } from '../../shared/header/header';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [Header, CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule,RouterLink],
   templateUrl: './register.html',
   styleUrls: ['./register.css']
 })
 export class RegisterComponent {
-  names = '';
-  surnames = '';
-  username = '';
-  email = '';
-  password = '';
-  showErrors = false;
 
-  onRegister() {
-    this.showErrors = true;
-    if (!this.names || !this.surnames || !this.username || !this.email || !this.password) {
-      return;
-    }
-    // Aquí iría la lógica de registro real
-    alert('¡Registro exitoso!');
-  }
 }
